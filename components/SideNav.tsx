6e96f9b026067acc7d0d21ae940602910a8ddbc1
Avi Avinav <aviavinav2004@gmail.com>
2023-04-11 12:25:56 +0530

    add under construction pages, fix sidenav

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import clsx from "clsx";
 import Link from "next/link";
 import { useCallback } from "react";
@@ -8,41 +6,35 @@ import { useStore } from "~/lib/store";
 
 const SideNav = () => {
   const isNavOpen = useStore(useCallback((state) => state.isNavOpen, []));
-  const setIsNavOpen = useStore(useCallback((state) => state.setIsNavOpen, []));
   return (
     <div
       className={clsx(
-        "max-h-screen min-w-screen text-gray-700 bg-emerald-200 -translate-x-full flex flex-row justify-between font-medium z-10 fixed top-0 left-0 right-0 bottom-0 px-10 sm:px-20 md:px-44",
+        "max-h-screen min-w-screen bg-emerald-200 -translate-x-full flex flex-row justify-between font-medium z-10 fixed top-0 left-0 right-0 bottom-0 px-10 sm:px-20 md:px-44",
         isNavOpen ? "translate-x-0" : "-translate-x-[calc(100% - 50px)])]",
         "transition-all duration-500 ease-in-out"
       )}
     >
       <div className="flex flex-col justify-between min-h-screen py-32">
         <ConsiousLink
-          className="text-[2.5rem] sm:text-5xl lg:text-6xl xl:text-7xl sm:leading-tight lg:leading-[1.10] xl:leading-[1.15]"
           href="/"
         >
           Home
         </ConsiousLink>
-        <Link
-          className="text-[2.5rem] sm:text-5xl lg:text-6xl xl:text-7xl sm:leading-tight lg:leading-[1.10] xl:leading-[1.15]"
+        <ConsiousLink
           href="/projects"
         >
           Projects
-        </Link>
-        <Link
-          className="text-[2.5rem] sm:text-5xl lg:text-6xl xl:text-7xl sm:leading-tight lg:leading-[1.10] xl:leading-[1.15]"
+        </ConsiousLink>
+        <ConsiousLink
           href="/blog"
-          onClick={setIsNavOpen}
         >
           Blog
-        </Link>
-        <Link
-          className="text-[2.5rem] sm:text-5xl lg:text-6xl xl:text-7xl sm:leading-tight lg:leading-[1.10] xl:leading-[1.15]"
-          href="/"
+        </ConsiousLink>
+        <ConsiousLink
+          href="/#footer"
         >
           Contact
-        </Link>
+        </ConsiousLink>
       </div>
       <div className="py-32">
         <Navbutton inNav />
@@ -52,14 +44,15 @@ const SideNav = () => {
 };
 
 interface ConsiousLinkProps {
-  className: string;
   href: string;
   children: React.ReactNode;
 }
 
-const ConsiousLink = ({ className, href, children }: ConsiousLinkProps) => {
+const ConsiousLink = ({ href, children }: ConsiousLinkProps) => {
+  const setIsNavOpen = useStore(useCallback((state) => state.setIsNavOpen, []));
+
   return (
-    <Link href={href} className={className}>
+    <Link onClick={setIsNavOpen} href={href} className={clsx("text-[2.5rem] text-gray-700 sm:text-5xl lg:text-6xl xl:text-7xl sm:leading-tight lg:leading-[1.10] xl:leading-[1.15]")}>
       {children}
     </Link>
   );
