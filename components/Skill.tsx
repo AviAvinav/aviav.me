8348610a3b37992e5bd6503564f2a3b256949b41
Avi Avinav <aviavinav2004@gmail.com>
2023-04-16 17:52:53 +0530

    add about me, change skills color

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -8,8 +8,8 @@ interface SkillProps {
 const Skill = ({ Icon, name }: SkillProps) => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center space-y-2">
-      <Icon className="w-full h-full" />
-      <p className="capitalize text-[1.5rem] sm:text-lg lg:text-xl xl:text-2xl font-medium">
+      <Icon className="w-full h-full text-gray-300" />
+      <p className="capitalize text-gray-300 text-[1.4rem] sm:text-lg lg:text-xl xl:text-2xl font-medium">
         {name}
       </p>
     </div>
