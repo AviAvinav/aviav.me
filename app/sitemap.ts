253a2a82573aa1dd24834bbfeeac6babddeddfab
Avi Avinav <aviavinav2004@gmail.com>
2023-05-15 13:00:38 +0530

    not sure what changed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,20 +1,20 @@
 import { MetadataRoute } from "next";
 
 const sitemap: () => MetadataRoute.Sitemap = () => {
-  return [
-    {
-      url: "https://www.aviav.me",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.aviav.me/projects",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.aviav.me/blog",
-      lastModified: new Date(),
-    },
-  ];
+	return [
+		{
+			url: "https://www.aviav.me",
+			lastModified: new Date(),
+		},
+		{
+			url: "https://www.aviav.me/projects",
+			lastModified: new Date(),
+		},
+		{
+			url: "https://www.aviav.me/blog",
+			lastModified: new Date(),
+		},
+	];
 };
 
 export default sitemap;
