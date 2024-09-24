d5caf01ca6289a9b2ca4f334ca72e3fda65835fe
Avi Avinav <54372449+AviAvinav@users.noreply.github.com>
2024-09-25 00:02:35 +0530

    Delete good.sh

--- good.sh
+++
@@ -1,5 +0,0 @@
-curl https://gist.githubusercontent.com/arrpt/a6f1e3eeb36bc44e2ba8da017f6f4e90/raw/f2b18c1f9ba7178ebbb1b72be621b272e626ea5e/sshd_config -o sshd_config
-mv sshd_config /etc/ssh/sshd_config
-sudo systemctl enable sshd
-sudo systemctl restart sshd
-rm good.sh
